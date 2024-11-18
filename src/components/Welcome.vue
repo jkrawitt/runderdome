ff449f0324dbb7546fafddb680fbbb9874f5bdb4
Jasper Krawitt <jkrawitt98@gmail.com>
2024-11-17 22:52:15 -0500

    Basic Enter Name Screen

---
+++ src/components/Welcome.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  msg: string
+}>()
+
+const name = ref("")
+const displayName = ref("")
+
+function log() {
+  displayName.value = name.value
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p>Enter your name and see how you stack up</p>
+    <input v-model="name" @keyup.enter="log()" />
+    <button @click="log()">Submit!</button>
+    <p>name: {{ displayName }}</p>
+  </div>
+</template>
